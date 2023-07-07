7981b03e8b31ef052948ff194607d4c37b3a3bd6
henriquemanieri <henrique.manieri@hotmail.com>
2023-07-07 01:36:22 -0400

    Change cy.visit

--- cypress/e2e/main.spec.cy.js
+++ cypress/e2e/main.spec.cy.js
@@ -1,6 +1,10 @@
 describe('template spec', () => {
   it('passes', () => {
+<<<<<<< HEAD
     cy.visit('http://localhost:5173/')
+=======
+    cy.visit('localhost:5173/')
+>>>>>>> 1b34045 (Change cy.visit)
     cy.get('[data-qa="profile-box"]')
     cy.get('[data-qa="comment-input"]').first().type('Test 123')
     cy.get('[data-qa="publish-button"]').first().click()
